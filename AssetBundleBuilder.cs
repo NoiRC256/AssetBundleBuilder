7b466ed174dcc45ea76da34f17487c40ff215b5d
NoiR_CCC <33998067+NoiRC256@users.noreply.github.com>
2020-03-22 16:44:51 +1100

    Add files via upload

---
+++ AssetBundleBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BundleBuilder : Editor
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+//Default output folder is C:\
+        BuildPipeline.BuildAssetBundles(@"C:\", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+    }
+}
